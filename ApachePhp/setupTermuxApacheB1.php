9ac40a7d211b5dc6d8c3a0ec9ecabad5e0d55256
programmerstandard <94773792+programmerstandard@users.noreply.github.com>
2026-08-22 07:26:07 +0700

    Create setupTermuxApacheB1.php

---
+++ ApachePhp/setupTermuxApacheB1.php
@@ -0,0 +1,18 @@
+# Install Apache, PHP, And Other Supported Packages
+
+## Preparation
+
+```bash
+apt update
+apt --dry-run upgrade
+apt upgrade
+```
+
+type `y` if needed.
+
+## Install Nano Text Editor
+
+```bash
+apt install --dry-run nano
+apt install nano
+```
